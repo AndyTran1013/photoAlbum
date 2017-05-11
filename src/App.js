bbf63ea747bb1a4dabb25ec5a9983a97e033ccec
Andy Tran <a.tran1013@gmail.com>
2017-05-10 23:03:33 -0400

    Working prototype 2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,104 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
+/*ToDo: Resize modal on window update? or just dump all styles in CSS*/
+
+class Modal extends React.Component {
+  render() {
+    let modalStyle={
+      display: this.props.display || "none"
+    };
+
+    return (
+      <div className="modal" style={modalStyle} onClick={this.props.onClick}>
+        <span
+          className="close-btn"
+          onClick={this.props.onClick}>
+          X
+        </span>
+        
+       <img className="modal-content" src={this.props.imgUrl} alt={this.props.imgUrl}/>
+      </div>
+    );
+  }
+}
+
+class ModalButtons extends React.Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <li>
+       <img 
+          className="modal-btn"
+          src={this.props.content} 
+          alt={this.props.content}
+          onClick={this.props.onClick}
+        />
+      </li>
+    );
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      boxes: [],
+      display: "none",
+      imgPath: ""
+    };
+  }
+
+  componentDidMount(){
+    let myRequest = new XMLHttpRequest();
+    let that = this;
+
+    myRequest.open("GET","/imgList",true);
+    myRequest.addEventListener('load',function(){
+      that.setState({boxes: JSON.parse(myRequest.response)['imgs']});
+    });
+    myRequest.send(null);
+
+    //used for testing
+    //this.setState({boxes: ['20160409_194910.jpg','20160410_070313.jpg']})
+  }
+
+  showModal(e, imgUrl) {
+    this.setState({
+      display: "block",
+      imgPath: './images/' + imgUrl
+    });
+  }
+
+  closeModal(e) {
+    if (e.target.className !== "modal-content") {
+      this.setState({
+        display: "none",
+        imgPath: ""
+      });
+    }
+  }
+
+  render() {
+
+    const btnBoxes=this.state.boxes.map((value, index)=>{
+      return (
+        <ModalButtons 
+          onClick={(e) => this.showModal(e,value)}
+          key={index}
+          content={'./images/thumbnails/' + value}
+        />
+      )
+    })
+
+
+    return (
+      <div>
+        <Modal
+          display={this.state.display}
+          onClick={e => this.closeModal(e)}
+          imgUrl={this.state.imgPath}
+        />
+        <ul className='list-inline'>{btnBoxes}</ul>
       </div>
     );
   }
